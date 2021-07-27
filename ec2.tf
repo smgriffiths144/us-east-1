c461d6e81c70ce616616390cd764517248c069bd
smgriffiths144 <79257270+smgriffiths144@users.noreply.github.com>
2021-07-27 14:58:15 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance"  "wordpress" {
-  ami           = "ami-0dc8f589abe99f538"
+  ami           = "ami-0dc2d3e4c0f9ebd18"
   instance_type = "t2.micro"
   associate_public_ip_address = true
 user_data = "${file("install_wordpress.sh")}"
